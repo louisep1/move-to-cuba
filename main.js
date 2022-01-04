a7441b82d646473da689c687139d02c9167c236d
louisep1 <louisepike@live.co.uk>
2022-01-04 12:15:00 +0900

    BIG JavaScript update

--- main.js
+++ main.js
@@ -18,97 +18,13 @@ const havanaBtn = document.getElementById('havana');
 const santaBtn = document.getElementById('santa');
 const islaBtn = document.getElementById('isla');
 
-// Property page popup - clickable dots and viewable imgs
-const slide1Img1 = document.querySelector(".pop-up .slide1 .imgs .img1")
-const slide1Img2 = document.querySelector(".pop-up .slide1 .imgs .img2")
-const slide1Img3 = document.querySelector(".pop-up .slide1 .imgs .img3")
-
-const slide1Dot1 = document.querySelector(".pop-up .slide1 .dots .dot1")
-const slide1Dot2 = document.querySelector(".pop-up .slide1 .dots .dot2")
-const slide1Dot3 = document.querySelector(".pop-up .slide1 .dots .dot3")
-
-
-
-
-
-
-
-
+// Dots and imgs node lists for 1st slide
+const slide1 = document.querySelectorAll('.pop-up .slide1 .imgs img');
+const dots1 = document.querySelectorAll('.pop-up .slide1 .dots .dot');
 
 
 // FUNCTIONS
-// highlight variable and remove highlight function
-const highlight = document.querySelector('.highlight');
-
-function removeHighlight() {
-  allBtns.forEach(item => item.classList.remove('highlight'));
-}
-
-// Functions for event listeners
-function openAll(e) {
-  removeHighlight();
-  this.classList.add('highlight')
-  for (let i = 0; i < allProperties.length; i++) {
-    allProperties[i].style.display = 'grid';
-  }
-}
-
-function openVillas(e) {
-  removeHighlight();
-  this.classList.add('highlight')
-  for (let i = 0; i < allProperties.length; i++) {
-    allProperties[i].style.display = 'none';
-  }
-  for (let i = 0; i < villas.length; i++) {
-    villas[i].style.display = 'grid';
-  }
-}
-
-function openApartments(e) {
-  removeHighlight();
-  this.classList.add('highlight')
-  for (let i = 0; i < allProperties.length; i++) {
-    allProperties[i].style.display = 'none';
-  }
-  for (let i = 0; i < apartments.length; i++) {
-    apartments[i].style.display = 'grid';
-  }
-}
-
-function openHavana(e) {
-  removeHighlight();
-  this.classList.add('highlight')
-  for (let i = 0; i < allProperties.length; i++) {
-    allProperties[i].style.display = 'none';
-  }
-  for (let i = 0; i < havana.length; i++) {
-    havana[i].style.display = 'grid';
-  }
-}
-
-function openSanta(e) {
-  removeHighlight();
-  this.classList.add('highlight')
-  for (let i = 0; i < allProperties.length; i++) {
-    allProperties[i].style.display = 'none';
-  }
-  for (let i = 0; i < santa.length; i++) {
-    santa[i].style.display = 'grid';
-  }
-}
-
-function openIsla(e) {
-  removeHighlight();
-  this.classList.add('highlight')
-  for (let i = 0; i < allProperties.length; i++) {
-    allProperties[i].style.display = 'none';
-  }
-  for (let i = 0; i < isla.length; i++) {
-    isla[i].style.display = 'grid';
-  }
-}
-
-
+// Stand-alone functions which don't call other (non pre-defined) functions within them
 // display/hide element function
 function displayBlock(elem) {
   elem.style.display = 'block';
@@ -117,13 +33,75 @@ function displayNone(...elems) {
   elems.forEach(elem => elem.style.display = 'none')
 }
 
+function displayNoneArr([...elems]) {
+  elems.forEach(elem => elem.style.display = 'none')
+}
+function displayGridArr([...elems]) {
+  elems.forEach(elem => elem.style.display = 'grid')
+}
+
 // Manipulating dots in pop-up section
 function addDot(dot) {
   dot.classList.add('current-dot')
 }
-function removeDot(...dots) {
+
+function removeDots([...dots]) {
   dots.forEach(dot => dot.classList.remove('current-dot'))
 }
+
+
+// highlight variable and remove highlight function
+const highlight = document.querySelector('.highlight');
+
+function removeHighlight() {
+  allBtns.forEach(item => item.classList.remove('highlight'));
+}
+
+
+// Functions for event listeners - these ALL call another function inside
+function resetFilter() {
+  removeHighlight();
+  displayNoneArr(allProperties)
+}
+
+function openAll(e) {
+  removeHighlight();
+  this.classList.add('highlight');
+  displayGridArr(allProperties);
+}
+
+function openVillas(e) {
+  // console.log("first: " + e)
+  // console.log("second: " + e.target)
+  resetFilter();
+  this.classList.add('highlight');
+  displayGridArr(villas);
+}
+
+function openApartments(e) {
+  resetFilter();
+  this.classList.add('highlight');
+  displayGridArr(apartments);
+}
+
+function openHavana(e) {
+  resetFilter();
+  this.classList.add('highlight');
+  displayGridArr(havana);
+}
+
+function openSanta(e) {
+  resetFilter();
+  this.classList.add('highlight');
+  displayGridArr(santa);
+}
+
+function openIsla(e) {
+  resetFilter();
+  this.classList.add('highlight');
+  displayGridArr(isla);
+}
+
 
 
 // EVENT LISTENERS
@@ -145,9 +123,9 @@ islaBtn.addEventListener('click', openIsla);
 
 
 
-// Property page "see more"
+// Property page "see more" pop-up and close functions and event listeners
 const popUpBtn = document.querySelector(".pop-up-btn");
-// later revise this as an array of all buttons and loop through to display correct one
+// // later revise this as an array of all buttons and loop through to display correct one
 
 const closeBtn = document.querySelector(".pop-up .close");
 
@@ -155,13 +133,15 @@ const popUp = document.querySelector(".pop-up");
 
 function openPopUp(e) {
   displayBlock(popUp);
-  displayBlock(slide1Img1);
-  addDot(slide1Dot1);
-  removeDot(slide1Dot2, slide1Dot3)
+  displayBlock(slide1[0]);
+
+  removeDots(dots1)
+  addDot(dots1[0]);
 }
 
 function closePopUp(e) {
-  displayNone(popUp, slide1Img1, slide1Img2, slide1Img3)
+  displayNone(popUp)
+  displayNoneArr(dots1)
 }
 
 popUpBtn.addEventListener('click', openPopUp);
@@ -173,29 +153,16 @@ closeBtn.addEventListener('click', closePopUp)
 
 
 
-// View images
-
-function showImg1(e) {
-  displayBlock(slide1Img1);
-  displayNone(slide1Img2, slide1Img3);
-  addDot(slide1Dot1);
-  removeDot(slide1Dot2, slide1Dot3)
+// View images function and event listener
+function scrollImgs(e) {
+  displayNoneArr(slide1)
+  removeDots(dots1)
+  for (let i = 0; i < dots1.length; i++) {
+    if (dots1[i] === this) {
+      displayBlock(slide1[i])
+      addDot(dots1[i])
+    }
+  }
 }
 
-function showImg2(e) {
-  displayBlock(slide1Img2);
-  displayNone(slide1Img1, slide1Img3);
-  addDot(slide1Dot2);
-  removeDot(slide1Dot1, slide1Dot3)
-}
-
-function showImg3(e) {
-  displayBlock(slide1Img3);
-  displayNone(slide1Img1, slide1Img2);
-  addDot(slide1Dot3);
-  removeDot(slide1Dot1, slide1Dot2)
-}
-
-slide1Dot1.addEventListener('click', showImg1);
-slide1Dot2.addEventListener('click', showImg2);
-slide1Dot3.addEventListener('click', showImg3);
+dots1.forEach(d => d.addEventListener('click', scrollImgs));
